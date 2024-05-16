de018862bc67dca274fc897b14a6b799eeb2b42b
hguler10 <160021724+hguler10@users.noreply.github.com>
2024-05-16 20:19:12 +0300

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -4,6 +4,9 @@ public class Book {
     private String title;
     private String author;
     private boolean isOverdue;
+    private boolean isAvailable;
+    private List<Rating> ratings;
+    private List<Member> reservationList;
 
     protected String getTitle(){
         return title;
@@ -22,5 +25,38 @@ public class Book {
     }
     protected void setisOverdue(boolean isOverdue){
         this.isOverdue=isOverdue;
+    }
+    protected List<Rating> getRatings(){
+    	return this.ratings;
+    }
+    protected void addRating(Rating rating) {
+    	this.ratings.add(rating);
+    }
+    protected double getAverageRating() {
+    	if(this.ratings.isEmpty()) {
+    		return 0;
+    	}else {
+    		double sum = 0;
+    		for (Rating rating : this.ratings) {
+    			sum += rating.getScore();
+			}
+    		return sum/this.ratings.size();
+    	}
+    }
+    protected void reserve(Member member) {
+    	if(!reservationList.contains(member)) {
+    		reservationList.add(member);
+    	}
+    }
+    protected void notifyMembers() {
+    	for (Member member : reservationList) {
+			member.notifyWhenAvailable(this);
+		}
+    	reservationList.clear();
+    }
+    protected void setAvailable(boolean available) {
+    	if(this.isAvailable) {
+    		notifyMembers();
+    	}
     }
 }
